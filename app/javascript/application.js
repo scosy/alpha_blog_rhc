213d7b50ea5ba83a71dd127eec497c1f83f49344
scosy <amine.bentaleb064@gmail.com>
2022-03-15 13:12:38 +0100

    Styling: navbar and jumbotron

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import './add_jquery'
 //= require jquery
 //= require bootstrap-sprockets
+//= require bootstrap.min
