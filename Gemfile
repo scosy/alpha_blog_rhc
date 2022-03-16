ef8b0aa151f069bc4760ad3129200328e5cfcb0a
scosy <amine.bentaleb064@gmail.com>
2022-03-16 13:44:58 +0100

    Added password to users

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby "3.0.0"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.2", ">= 7.0.2.3"
-
+gem 'bcrypt', '~> 3.1.7'
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_16_114454) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_16_123705) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_16_114454) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "users"
