f7db777ad6bb0d2b870877c55057852857838368
scosy <amine.bentaleb064@gmail.com>
2022-03-16 09:19:36 +0100

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
 	has_many :comments
+	validates :username, presence: true,
+						uniqueness: { case_sentitive: false },
+						length: {minimum: 3, maximum: 25}
+	VALID_EMAIL_REGEX = /.+\@.+\..+/
+	validates :email, presence: true,
+										uniqueness: { case_sentitive: false },
+										format: { with: VALID_EMAIL_REGEX }
 end
