f06fdbd1736060dacb1c5d2fefc9f79b29c043ae
scosy <amine.bentaleb064@gmail.com>
2022-03-17 12:56:37 +0100

    Create and destroy actions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   resources :comments
+  
   resources :users, excepet: [:new]
+  
   resources :articles
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "pages#about"
+  
   get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
   # Defines the root path route ("/")
   # root "articles#index"
   root "pages#home"
